2da256a7bf984b1965ae27ba2de50220370010d5
Bence Hornák <bence.hornak@gmail.com>
2021-02-08 09:39:53 +0100

    Generate APP_KEY

--- docker-envault-entrypoint.sh
+++ docker-envault-entrypoint.sh
@@ -1,7 +1,26 @@
 #!/bin/sh
 set -e
 
-# php artisan key:generate
+################################################################################
+# Generate APP_KEY, if variable is not set
+################################################################################
+if [ -z "$var" ]
+then
+	# Create a placeholder in .env
+	echo APP_KEY= > .env
+	# Generate key
+	php artisan key:generate -q
+	echo "**************************************\n*     Application Key Generated!     *\n**************************************"
+	# Print APP_KEY
+	cat .env
+fi
+
+################################################################################
+# Run migrations
+################################################################################
 echo yes | php artisan migrate
 
+################################################################################
+# Call the original entrypoint and pass all variables
+################################################################################
 docker-php-entrypoint "$@"
